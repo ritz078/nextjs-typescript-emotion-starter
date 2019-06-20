ac6c67d24d037e4ca68fc1a58d5e3c7b3e4d6c97
ritz078 <rkritesh078@gmail.com>
2019-06-20 19:44:02 +0530

    feat(root): add absolute path example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,8 @@
 import React from "react";
+import { wrapper } from "@styles/main.styles";
 
 const MainApp: React.FunctionComponent<{}> = () => {
-  return (
-    <h1
-      css={{
-        color: "green"
-      }}
-    >
-      I am working
-    </h1>
-  );
+  return <h1 css={wrapper}>I am working</h1>;
 };
 
 export default MainApp;
